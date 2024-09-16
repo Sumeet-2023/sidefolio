1f3d264487e1e63bbe69720ed9473f35152867a4
Sumeet Mourya <go69ciq@mytum.de>
2024-09-17 00:26:10 +0200

    few changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,13 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Sumeet</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        I want to be a full-stack developer that loves{" "}
+        <Highlight>building products</Highlight> and web apps.
       </Paragraph>
-      
+      <Paragraph className="max-w-xl mt-4">
+        I have been building scalable web apps that are performance optimised and stunning design for{" "}
+        <Highlight>more than 6 months.</Highlight>
+      </Paragraph>
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -43,47 +43,30 @@ export default function About() {
 
       <div className="max-w-4xl">
         <Paragraph className=" mt-4">
-          Hey there, I&apos;m Sumeet Mourya - a passionate developer, avid writer,
-          and a connoisseur of awesome design. Welcome to my corner of the
+          Hey there, I&apos;m Sumeet Mourya - a passionate developer. Welcome to my corner of the
           digital world!
         </Paragraph>
         <Paragraph className=" mt-4">
-          Since the early days of my journey, I&apos;ve been captivated by the
-          art of crafting exceptional digital experiences. As a developer, I
-          thrive on turning lines of code into functional and elegant solutions.
-          My goal is to not just create software, but to build digital marvels
-          that seamlessly merge form and function.
+          Since the early days of my journey, I&apos;ve been captivated by the art
+          of crafting exceptional digital experiences. As a developer,
+          I thrive on turning lines of code into functional and elegant solutions.
+          My goal is to create extensive and scalable backend systems along with smooth UI
         </Paragraph>
 
         <Paragraph className=" mt-4">
-          But my journey doesn&apos;t stop at coding. With a heart full of words
-          and a mind brimming with ideas, I&apos;ve ventured into the realm of
-          writing. From tech articles that unravel complex concepts to creative
-          tales that ignite the imagination, I weave words to inform, entertain,
-          and inspire.
+          I know <span className="text-yellow-400">{" "}Java and Typescript</span> along with technologies like
+          <span className="text-blue-400">{" "}Next js, React js, Express, Databases like Postgress, Mongo db and Redis.
+            I&apos;m also bit comfortable in Machine Learning and AI</span>.
         </Paragraph>
         <Paragraph className=" mt-4">
-          What sets me apart is my unwavering appreciation for design. I believe
-          that aesthetics and usability go hand in hand. My eye for awesome
-          design ensures that every project I undertake not only works
-          flawlessly under the hood but also looks stunning on the surface.
+          What sets me apart is my unwavering appreciation for design.
+          I believe that aesthetics and usability go hand in hand.
+          My eye for awesome design ensures that every project I undertake
+          looks stunning on the surface for that I use <span className="text-blue-400">{" "}Tailwind CSS.</span>
         </Paragraph>
         <Paragraph className=" mt-4">
-          Through this website, I aim to share my insights, experiences, and
-          creations with you. Whether you&apos;re a fellow developer seeking
-          solutions, a fellow writer in search of inspiration, or simply someone
-          who appreciates the finer aspects of design, there&apos;s something
-          here for you.
-        </Paragraph>
-        <Paragraph className=" mt-4">
-          Join me on this journey of bytes and narratives, logic and creativity,
-          code and prose. Together, we can explore the boundless possibilities
-          of technology and storytelling, all while reveling in the sheer beauty
-          of thoughtful design.
-        </Paragraph>
-        <Paragraph className=" mt-4">
-          Thank you for being here, and I can&apos;t wait to embark on this
-          adventure with you.
+        If you are looking for someone with great backend and frontend understanding, Hit me up.
+        Thanks for reading this and navigating through my creation.
         </Paragraph>
       </div>
     </div>
